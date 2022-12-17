95e4f598a2ca0409de711d5b082e3f85a25a8066
Lasse Wardenaer <lasse.wardenar@gmail.com>
2022-12-17 21:34:30 +0100

    initalize dashboard application

---
+++ monitorDashBoard/monitordashboard/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+      370 Rue Jean Belleville Statistics
+      </header>
+    </div>
+  );
+}
+
+export default App;
