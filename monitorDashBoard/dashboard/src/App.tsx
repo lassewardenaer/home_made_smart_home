d9fd08b67e2b61c1d0f8d54bc6fa8a4f94ba5428
Lasse Wardenaer <lasse.wardenar@gmail.com>
2022-12-17 23:17:52 +0100

    build init dir

--- monitorDashBoard/dashboard/src/App.tsx
+++ monitorDashBoard/dashboard/src/App.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="header">
-      370 Rue Jean Belleville Statistics
+      <header className="App-header">
+        370 Rue Jean Belleville Statistics
       </header>
     </div>
   );
