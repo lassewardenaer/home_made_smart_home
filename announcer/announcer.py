e9754cae50a67be27b35d10ace9bc1d43c88102e
Lasse Wardenaer <lasse.wardenar@gmail.com>
2022-12-17 17:14:06 +0100

    add mixer to announcer

--- announcer/announcer.py
+++ announcer/announcer.py
@@ -1,7 +1,35 @@
 import argparse
 from gtts import gTTS 
 import os
+from pygame import mixer
 
+#v---------- sayText ---------- #
+def sayText(text, fileName="temp.mp3"):
+    tts = gTTS(line)
+    tts.save(fileName)
+
+    mixer.init()
+    mixer.music.load(filename)
+    mixer.music.play()
+
+    seconds = 0
+    while mixer.music.get_busy() == 1:
+        time.sleep(0.25)
+        seconds += 0.25
+    mixer.quit()
+    os.remove(fileName)
+
+#v---------- getText ---------- #
+def getText(textFile):
+    with open(textFile) as f:
+        lines = f.readlines()
+    
+    for line in lines:
+       sayText(line)
+    
+    f.close()
+
+#v---------- main ---------- #
 def main():
     tts = gTTS('hello')
 
